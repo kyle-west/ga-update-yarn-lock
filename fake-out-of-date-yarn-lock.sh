1c87a30578edf1c09a0277742d495025d7112136
Kyle West <kyle@familysearch.org>
2020-10-02 10:01:55 -0600

    test out default token

--- fake-out-of-date-yarn-lock.sh
+++ fake-out-of-date-yarn-lock.sh
@@ -1,8 +1,8 @@
-sed -i '' 's/dotenv": "^8.2.0/dotenv": "8.1.0/g' package.json
+sed -i '' 's/dotenv": "^8.1.0/dotenv": "8.1.0/g' package.json
 sleep 1
 rm -rf node_modules yarn.lock
 sleep 1
 yarn install
 sleep 1
 sed -i '' 's/dotenv@8.1.0:/dotenv@^8.1.0:/g' yarn.lock
-sed -i '' 's/dotenv": "8.1.0/dotenv": "^8.2.0/g' package.json
+sed -i '' 's/dotenv": "8.1.0/dotenv": "^8.1.0/g' package.json
